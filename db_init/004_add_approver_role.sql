134b54a08f223bdd16d1433926c858949ac80581
Krisaor <44208831+rodolfobusnardo@users.noreply.github.com>
2025-06-20 13:07:31 -0300

    Delete db_init/004_add_approver_role.sql

--- db_init/004_add_approver_role.sql
+++
@@ -1,4 +0,0 @@
--- Add is_donation_approver column to users table
-ALTER TABLE `users`
-ADD COLUMN `is_donation_approver` BOOLEAN NOT NULL DEFAULT FALSE
-COMMENT 'Flag to indicate if the user can approve donations';
